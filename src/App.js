69637ef196105db439a159afcd7a799711abc9e9
DBPerera22 <denukaperera49@gmail.com>
2022-12-18 01:41:48 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+
+  const [movieName, setMovieName] = useState('')
+   const [review, setReview] = useState('')
+
+   const submitReview = () => {
+    Axios.post("http://localhost/api/insert",{movieName: movieName, movieReview: review}).then(()=>{
+      alert("Successfully inserted");
+    })
+   };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fisherman Registration</h1>
+
+      <div className='form'>
+      <label> Name: </label>
+        <input type='text' className='name' onChange={(e)=> {
+          setMovieName(e.target.value)
+        }} />
+      <label> Review: </label>
+        <input type='text' className='review' onChange={(e)=> {
+          setReview(e.target.value)
+        }} />
+        <button onClick={submitReview}>Submit</button>
+      </div>
+
+
     </div>
   );
 }
